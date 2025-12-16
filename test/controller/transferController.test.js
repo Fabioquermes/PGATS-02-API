a3592fc451e27340ea6b3ffaabbec01918f284cb
Fabio Quermes <fabiomobi@gmail.com>
2025-12-15 21:01:29 -0300

    Adicionando um movo teste, utilizando o mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -1,15 +1,20 @@
 // Dependencias ou Bibliotecas
-
 const request = require('supertest');
 const sinon = require('sinon');
 const { expect } = require('chai');
 
 // Aplicacao
 const app = require('../../app');
+
+// Mocks
+const transferService = require('../../services/transferService');
+const userService = require('../../services/userService');
+const Sinon = require('sinon');
+
 // Testes
 describe('Transfer Controller', () => {
     describe('POST /transfers', () => {
-        it('Quando informa remetente e destinatario inexistente recebo 400', async () => {
+        it('Quando informa remetente e destinatario inexistente recebo 404', async () => {
             const resposta = await request(app)
                 .post('/transfers')
                 .send({
@@ -19,7 +24,27 @@ describe('Transfer Controller', () => {
                 });
             expect(resposta.status).to.equal(404);
             expect(resposta.body).to.have.property('message', 'Usuário remetente ou destinatário não encontrado.');           
-        });    
+        });
+        
+        it('Usando Mocks: Quando informa remetente e destinatario inexistente recebo 404', async () => {
+            // Mock do serviço de transferência
+            const transferMock = sinon.stub(transferService, 'transfer');
+            transferMock.throws({ status: 404, message: 'Usuário remetente ou destinatário não encontrado.' });
+
+
+            const resposta = await request(app)
+                .post('/transfers')
+                .send({
+                    remetente: "fabio",
+                    destinatario: "priscila",
+                    valor: 100  
+                });
+            expect(resposta.status).to.equal(404);
+            expect(resposta.body).to.have.property('message', 'Usuário remetente ou destinatário não encontrado.');
+            
+            // Restaura o comportamento original do serviço após o teste
+            sinon.restore();
+        });
     });
 
     describe('GET /transfers', () => {
