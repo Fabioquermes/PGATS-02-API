a3852f24f6906c1d5cf3b3b4551df4f14703915c
Fabio Quermes <fabiomobi@gmail.com>
2025-12-15 22:53:43 -0300

    Refatoracao do codigo, para cobrir cenario de sucesso

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const userController = require('./controllers/userController');
-const transferController = require('./controllers/transferController');
+const createTransferController = require('./controllers/transferController');
+const transferService = require('./services/transferService');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/users', userController);
-app.use('/transfers', transferController);
+app.use('/transfers', createTransferController(transferService));
 
 module.exports = app;
